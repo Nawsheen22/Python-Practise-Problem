11a265d6fa3e4314fc920522e2077e7e33f14204
Nawsheen22 <nawsheensalsabeel18@gmail.com>
2024-11-03 23:27:27 +0600

    Add Problem00.py file

---
+++ Problem00.py
@@ -0,0 +1,40 @@
+#Write a program that will display the last digit of the number 
+
+
+
+
+
+'''number=int(input("Enter a number:"))
+print("The last digit of the number:",number%10)
+
+
+
+
+#Write a program to check if the number entered by the user is divisible by 3 or not 
+
+
+
+number1=int(input("Enter a number:"))
+if number1%3==0:   # while dividing by 3 if reminder is 0
+   print("Number is divisible by 3")
+else :
+   print("Not divisible by 3")'''
+
+
+
+#Problem a bit tricky
+
+Obtained_mark= int(input("Enter the number of the mark:"))
+
+
+
+if Obtained_mark > 90 :
+   print("A")
+elif Obtained_mark >80 and Obtained_mark <= 90:
+   print('B')
+elif Obtained_mark >=60 and Obtained_mark <=80:
+   print("C")   
+else:
+   print("D")   
+
+
