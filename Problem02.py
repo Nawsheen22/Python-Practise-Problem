8487c62301c5990f65fc44675f73ea21a90923aa
Nawsheen22 <nawsheensalsabeel18@gmail.com>
2024-11-03 23:59:32 +0600

    Add Pproblem02.py file

---
+++ Problem02.py
@@ -0,0 +1,16 @@
+number=int(input("Enter the number:"))
+
+if number==1:
+    print("Sunday")
+elif number==2:
+    print("Monday")
+elif number==3:
+    print("Tuesday")
+elif number==4:
+    print("Wednesday")
+elif number==5:
+    print("Thursday")
+elif number==6:
+    print("Friday")
+elif number==7:
+    print("Saturday")                        
