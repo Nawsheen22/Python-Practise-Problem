1e6edccf577b7af8d2874ffb8af5142b173923f8
Nawsheen22 <nawsheensalsabeel18@gmail.com>
2024-11-05 22:55:57 +0600

    Add Problem06.py file

---
+++ Problem06.py
@@ -0,0 +1,15 @@
+color_list=["pink","purple","green","dark blue"]
+color_list.append("Yellow")
+print(color_list)
+
+
+color_list.insert(2,"Orange")
+print(color_list)
+
+color_list.remove("dark blue")
+print(color_list)
+
+
+color_list[4]="Magenta"
+print(color_list)
+
