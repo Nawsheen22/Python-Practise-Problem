8fd3fde390e97b75d5db0cbd4373dff70a0b5f11
Nawsheen22 <nawsheensalsabeel18@gmail.com>
2024-11-17 11:02:00 +0600

    Add Problem12.py file

---
+++ Problem12.py
@@ -0,0 +1,19 @@
+#list
+
+colours=["Pink","Blue","Yellow","Orange","Magenta","Pink"] #allows duplicate
+
+print(colours)
+
+#incase if I have to check the length
+
+
+colours1=["Pink","fruit","heart","Cat","Khittie"]
+
+print(len(colours1))
+
+#A list can have boolean , string all data types together
+
+colours2=["Chocolate",25,True,False]
+
+print(colours2)
+
