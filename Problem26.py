c3de6ca26820762e8c59fa6c409057a611b28760
Nawsheen Salsabeel <125694725+Nawsheen22@users.noreply.github.com>
2026-07-13 13:20:03 +0600

    Add Problem26.py file

---
+++ Problem26.py
@@ -0,0 +1,17 @@
+#Q2. User Introduction Program
+
+
+#1)My name as User Input
+Name = input("Enter your name: ")
+
+#2)My city as User Input
+City = input("Enter your city: ")
+
+#3)My hobby as User Input
+Hobby = input ("What's your hobby?")
+
+print(f"\nHi !! My name is {Name}. I live in {City} and my hobby is {Hobby}.")
+
+
+
+
