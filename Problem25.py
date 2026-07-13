e2ec1546555b90a40450e326474e41943930b3d7
Nawsheen Salsabeel <125694725+Nawsheen22@users.noreply.github.com>
2026-07-13 12:48:01 +0600

    Add Problem25.py file

---
+++ Problem25.py
@@ -0,0 +1,41 @@
+#Q1. Simple Calculator Program
+#Solution:
+
+#Asking the user to input two numbers
+
+Number1 = input("Enter the First Number:")
+Number2 = input ("Enter the Second Number:")
+#Converting both numbers into float
+
+Number1 = float(Numer1)
+Number2 = float(Numer2)
+
+#Addition between two numbers
+
+Add=Numer1+Numer2
+
+#Subtraction between two numbers
+
+Sub=Numer1-Numer2
+
+#Multiplication between two numbers
+
+Mul=Numer1*Numer2
+
+#Division between two numbers
+
+Div=Numer1/Numer2
+
+
+#Printing the output of the results clearly
+
+
+print ("\nCalculation Results:")
+print(f"\n1) Addition: {Numer1} + {Numer2} = {Add}")
+print(f"2) Subtraction: {Numer1} - {Numer2} = {Sub}")
+print(f"3) Multiplication: {Numer1} * {Numer2} = {Mul}")
+print(f"4) Division: {Numer1} / {Numer2} = {Div}")
+
+
+
+
