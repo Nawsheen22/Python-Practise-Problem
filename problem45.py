bee2ee43e4b2a434fee142250a07d5942d3f47d0
Nawsheen Salsabeel <125694725+Nawsheen22@users.noreply.github.com>
2026-07-19 22:40:45 +0600

    add problem45.py

---
+++ problem45.py
@@ -0,0 +1,19 @@
+#Question 4: Set Operations
+
+#Given the sets:
+
+#set1 = {1, 2, 3, 4, 5}
+
+#set2 = {4, 5, 6, 7}
+
+#Print the common elements using set operation
+
+#Print the unique elements of set1
+
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7}
+
+print("Common elements:", set1 & set2)
+
+print("Unique elements of set1:", set1 - set2)
