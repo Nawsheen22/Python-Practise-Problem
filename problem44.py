9f40549cdc4a7ab346f7d313c5a2e0ed4aa7327a
Nawsheen Salsabeel <125694725+Nawsheen22@users.noreply.github.com>
2026-07-19 22:38:43 +0600

    add problem44.py

---
+++ problem44.py
@@ -0,0 +1,12 @@
+#Question 3: Tuple and Loop
+
+
+#Create a tuple of 5 subjects.
+
+#Using a for loop, print each subject one by one.
+
+
+subjects = ("Math", "Physics", "Chemistry", "English", "Biology")
+
+for subject in subjects:
+    print(subject)
