ab390e1dac3337e89acb831bd30030ac7e84cd62
Nawsheen Salsabeel <125694725+Nawsheen22@users.noreply.github.com>
2026-07-20 22:48:53 +0600

    add problem49.py

---
+++ problem49.py
@@ -0,0 +1,30 @@
+
+
+"""
+*In Python, functions are defined using the def keyword.
+
+*After def, we write the function name, followed by parentheses () and a colon :.
+
+*The code inside the function must be indented (spaced).
+
+*Running a function is called function calling.
+
+*Basic Syntax
+
+"""
+
+
+def function_name():
+    codeblock
+
+    # This does not do anything yet
+def greet():
+    print("Hello World")  # This won't run because it's only defined
+
+
+    greet()  # prints "Hello World" , cause the funtion is called here
+    def say_hi(): # function definition
+  """
+  input: nothing
+  """
+  print("hello Sazzad!")
